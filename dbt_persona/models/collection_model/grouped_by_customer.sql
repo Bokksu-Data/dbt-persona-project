a346321c7d4281320cae369a0b0f7989b1b80bd2
Ayesha <ayeshazaker@gmail.com>
2025-01-21 17:16:05 -0500

    fixed referencing

--- dbt_persona/models/collection_model/grouped_by_customer.sql
+++ dbt_persona/models/collection_model/grouped_by_customer.sql
@@ -1,6 +1,9 @@
 {{ config(materialized='table') }}
 
-WITH flavor_categories AS (
+WITH 
+
+products AS 
+(
 SELECT * from {{ref('int__collection_cleaning')}}
 ),
 
@@ -9,9 +12,12 @@ SELECT * from {{ref('int__collection_cleaning')}}
 customer_collections AS (
 SELECT 
 l.composite_customer_id, 
-cc.collection_title, 
-fc.flavor as flavors, 
-fc.brand as brand_name,
+l.composite_product_variant_id,
+l.lineitem_sku,
+brand,
+flavor_type,
+p.COLLECTION_NAMES_ARRAY,
+
 COUNT(DISTINCT l.composite_order_id) as total_orders,
 COUNT(DISTINCT l.composite_customer_id) as unique_customers,
 SUM(l.lineitem_quantity) as total_units_sold,
@@ -19,7 +25,8 @@ SUM(l.lineitem_net_revenue_at_quantity) as total_spent,
 MIN(l.CREATED_AT_EST::date) as first_order_date,
 MAX(l.CREATED_AT_EST::date) as most_recent_order_date
 
-FROM bokksu._core.fct__all__lineitems l LEFT JOIN flavor_categories fc ON l.composite_product_variant_id = fc.composite_product_variant_id
+FROM bokksu._core.fct__all__lineitems l 
+LEFT JOIN products as p ON l.composite_product_variant_id = p.composite_product_variant_id
 
 --filtering
 WHERE 
@@ -28,18 +35,22 @@ l.cancelled_at_est is null and
 l.lineitem_net_revenue_at_quantity > 0 and 
 l.composite_customer_id IS NOT NULL 
 
-GROUP BY l.composite_customer_id, cc.collection_title, flavors, brand_name
+GROUP BY 1,2,3,4,5,6
 )
 
-SELECT 
-composite_customer_id,
-collection_title, flavors, brand_name,
-COUNT(DISTINCT collection_title) as number_of_collections_bought,
-SUM(total_units_sold) as total_units_sold_all_collections,
-SUM(total_spent) as total_spent_all_collections,
-MIN(first_order_date) as first_ever_order,
-MAX(most_recent_order_date) as most_recent_order
+select * from customer_collections
 
-FROM customer_collections
-GROUP BY composite_customer_id, collection_title, flavors, brand_name
-ORDER BY total_spent_all_collections DESC
+-- SELECT 
+-- composite_customer_id,
+-- product_variant_id, 
+-- flavor_type,
+--  brand_name,
+-- COUNT(DISTINCT collection_title) as number_of_collections_bought,
+-- SUM(total_units_sold) as total_units_sold_all_collections,
+-- SUM(total_spent) as total_spent_all_collections,
+-- MIN(first_order_date) as first_ever_order,
+-- MAX(most_recent_order_date) as most_recent_order
+
+-- FROM customer_collections
+-- GROUP BY 1,2,3,4,5
+-- ORDER BY total_spent_all_collections DESC
